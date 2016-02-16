8517062ce803a7d672dd6dcaccbc7deaf5a1741c
Jacob Lambert <Jml171@zips.uakron.edu>
2016-02-16 15:00:41 -0500

    ADC Sample Ready

--- Motor Controls/adc_sample.c
+++ Motor Controls/adc_sample.c
@@ -11,6 +11,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+void ADC_write ()
+    if (write(file,buf,1) != 1)
+    {
+        /* ERROR HANDLING: i2c transaction failed */
+        printf("Failed to write to the i2c bus.\n");
+        buffer = g_strerror(errno);
+        printf(buffer);
+        printf("\n\n");
+    }
+
 void
 sensors_ADC_init(void) {
     int file;
@@ -53,13 +63,47 @@ sensors_ADC_init(void) {
 
     //unsigned char reg = 0x10; // Device register to access
     //buf[0] = reg;
-    buf[0] = 0b11110000;
+    //ads1015 datasheet settings pg 16
+    buf[0] = 0b10010000; //Writing to Config Register: Address
+    ADC_write ();
+    buf[0] = 0b00000001; //Points to the config register
+    ADC_write ();
+    buf[0] = 0b10000101; //Bits 15-8, Single Conversion, dafult multiplexer config, default voltage range (+-2.048)
+    ADC_write ();
+    buf[0] = 0b10000011; //bits 7-0, Comparator settings, comparator disabled
+    ADC_write ();
 
-    if (write(file,buf,1) != 1) {
-        /* ERROR HANDLING: i2c transaction failed */
-        printf("Failed to write to the i2c bus.\n");
-        buffer = g_strerror(errno);
-        printf(buffer);
-        printf("\n\n");
-    }
+}
+
+int adc_read()
+{
+    char buf[10] = {0};
+    float data;
+    char channel;
+
+    for(int i = 0; i<4; i++)
+        {
+        // Using I2C Read
+            if (read(file,buf,2) != 2)
+            {
+            /* ERROR HANDLING: i2c transaction failed */
+            printf("Failed to read from the i2c bus.\n");
+            buffer = g_strerror(errno);
+            printf(buffer);
+            printf("\n\n");
+            }
+            else
+            {
+            data = (float)((buf[0] & 0b00001111)<<8)+buf[1];
+            data = data/4096*3.3;
+            channel = ((buf[0] & 0b00110000)>>4);
+            printf("Channel %02d Data:  %04f\n",channel,data);
+            }
+}
+
+int main()
+{
+sensors_ADC_init();
+adc_read();
+
 }
