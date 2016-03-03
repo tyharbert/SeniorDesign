5ff9b62814673385d6f0137941e98e64659b8169
Joshua Kohut <jakoh77@yahoo.com>
2016-03-03 15:27:20 -0500

    increased speed for image transmissin

--- Xbee/xbeeRead.c
+++ Xbee/xbeeRead.c
@@ -2,27 +2,46 @@
 #define XSERIAL_HPP
 
 #include "xSerial.hpp"
+#include <wiringSerial.h>
+#include <errno.h>
 //#include <stdio.h>
 
 int main(){
-
+/*
         char *device = (char *)"/dev/ttyUSB0";
 
-        Serial xbee(device, 9600);
+        Serial xbee(device, 115200);
         xbee.Open();
+*/
 	char temp;
+	int fd;
+  if ((fd = serialOpen ("/dev/ttyUSB0", 57600)) < 0)
+  {
+    printf ("Unable to open serial device:\n");
+    return 1 ;
+  }
+
+
+//        int fd = serialOpen ("/dev/ttyUSB0", 115200);
+
+
 for(;;)
 {
-    	while (xbee.DataAvail() > 0)
+//    	while (xbee.DataAvail() > 0)
+	while (serialDataAvail(fd) > 0)
     	{
-	  temp = xbee.GetChar();
+	  temp = serialGetchar(fd);
+//	  temp = xbee.GetChar();
 	  std::cout << temp;
 //	  putchar ( xbee.GetChar());
 //    	  xbee.FlushData();
 	  fflush(stdout);
     	}
 }
-xbee.Close();
+
+serialClose(fd);
+
+//xbee.Close();
 return 0;
 }
 
--- Xbee/xbeeTest.c
+++ Xbee/xbeeTest.c
@@ -1,6 +1,6 @@
 #ifndef XSERIAL_HPP
 #define XSERIAL_HPP
-
+#include <wiringSerial.h>
 #include "xSerial.hpp"
 
 
@@ -8,10 +8,23 @@ int main(){
 
 	char *device = (char *)"/dev/ttyUSB1";
 
-	Serial xbee(device, 9600);
+  int fd;
+  if ((fd = serialOpen ("/dev/ttyUSB1", 57600)) < 0)
+  {
+    printf ("Unable to open serial device:\n");
+    return 1 ;
+  }
+
+
+//	int fd = serialOpen ("/dev/ttyUSB1", 115200);
+
+//	Serial xbee(device, 115200);
 //	xbee.Open();
-	xbee.PutMsg("This is a test");
+//	xbee.PutMsg("This is a test");
 //	xbee.Close();
+	serialPuts (fd, "this is a test");
+	printf("this is a test\n");
+	serialClose(fd);
 	return 0;
 
 
