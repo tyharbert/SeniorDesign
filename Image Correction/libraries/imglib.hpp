f073d5580d96f661c37957ee4a65c87490d28893
Tyler Harbert <harbertt11@gmail.com>
2016-03-11 15:12:05 -0500

    updating image correction cmake and library

---
+++ Image Correction/libraries/imglib.hpp
@@ -0,0 +1,9 @@
+#ifndef IMGLIB_HPP
+#define IMGLIB_HPP
+
+#include "bmp.hpp"
+#include "matrix.hpp"
+
+void transformGusset(const char*, const char*);
+
+#endif
--- Image Correction/main.cpp
+++ Image Correction/main.cpp
@@ -1,30 +1,8 @@
-#include "libraries/bmp.hpp"
-#include "libraries/matrix.hpp"
+#include "libraries/imglib.hpp"
 
 int main()
 {   
-   BMP* bmp = new BMP("../images/1.bmp");
-   Corners original = bmp->fast();
-
-   Corners destination = original.findDest();
-
-   original.print();
-   destination.print();
-
-   Matrix<float> U(original, destination);
-   Matrix<float> L(U);
-   Matrix<float> B(destination);
-
-   Matrix<float> P = U.lu();
-   L.lu(false);
-
-   Matrix<float> Y = L.forward_sub(P*B);
-   Matrix<float> H = U.back_sub(Y);
-
-   H.reshape(3, 3);
-
-   BMP* final = new BMP(bmp, H, original, destination);
-   final->write("../images/2.bmp");
+   transformGusset("../../images/test_in.bmp", "../../images/test_out.bmp");
 
     return 0;
 }
