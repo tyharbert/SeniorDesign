232eb0e87cc3d480acfc5a913d8e19b91b6c5575
tyharbert <harbertt11@gmail.com>
2016-03-15 18:30:58 +0000

    making const in .h file static

--- motorcontrols/libraries/servo.h
+++ motorcontrols/libraries/servo.h
@@ -11,7 +11,7 @@ void Pan_Gusset(int Pan_Loc, int Lower_Bound, int Upper_Bound);
 void Mov_Motor(int Motor_Num, int Motor_Loc);
 void Tilt_Gusset(int Tilt_Loc, int Lower_Bound, int Upper_Bound);
 
-extern const unsigned char butPin = 18; // Active something
+static const unsigned char butPin = 18; // Active something
 
 // Write up an equation to convert a feedback value to a corresponding pulse width. We will be taking in a feedback
 // value when setting up the gusset plate locations.
