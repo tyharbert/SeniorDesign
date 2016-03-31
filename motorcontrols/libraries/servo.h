f1269aa37298959a6cf9d3d7ac4bdc04e1e6fd11
Tyler Harbert <harbertt11@gmail.com>
2016-03-31 15:04:36 -0400

    adding functionality to read locations file in servo program

--- motorcontrols/libraries/servo.h
+++ motorcontrols/libraries/servo.h
@@ -5,14 +5,22 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
+static const int buffSize = 1024;
+// this is twice the size because it includes both servo positions
+// this is hard coded into captureLocation() of calibration.cpp
+static const int maxPositions = 8;
+
+static const unsigned char butPin = 18; // Active something
+
 unsigned short ADC_Rd(unsigned short address);
 unsigned short Rd_Rev(unsigned short);
 void Pan_Gusset(int Pan_Loc, int Lower_Bound, int Upper_Bound);
 void Mov_Motor(int Motor_Num, int Motor_Loc);
 void Tilt_Gusset(int Tilt_Loc, int Lower_Bound, int Upper_Bound);
 int FB_to_PW_Conv(int Servo);
-
-static const unsigned char butPin = 18; // Active something
+int* getPositions(int* length);
+int numPositions(char buffer[buffSize]);
+int nextPosition(char buffer[buffSize]);
 
 // Write up an equation to convert a feedback value to a corresponding pulse width. We will be taking in a feedback
 // value when setting up the gusset plate locations.
