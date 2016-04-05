30c01a506e36d080a957bea9a81127ca58b4c4a5
Tyler Harbert <harbertt11@gmail.com>
2016-04-05 15:49:51 -0400

    changing save location

--- motorcontrols/libraries/servo.c
+++ motorcontrols/libraries/servo.c
@@ -210,11 +210,11 @@ void Mov_Motor(int Motor_Num, int Motor_Loc) //Motor number (0 or 1), and Motor 
 
 void Cap_Image()
 {
-    int n=65;
+    int n=75;
     int cx=0;
     static int i=0;
     char command[n];
-    cx=snprintf(command, n, "fswebcam -r 2592x1944 --jpeg 100 -D 15 -S 13 1 testing%d.jpeg", i); //assigns the echo call as the command, with the limit of n characters
+    cx=snprintf(command, n, "fswebcam -r 2592x1944 --jpeg 100 -D 15 -S 13 1 ../images/testing%d.jpeg", i); //assigns the echo call as the command, with the limit of n characters
     if(cx>n)
         printf("Command Length Too Long");
     else
