09e2b7c24cc6b9a09a8243599b0d49e5e5ddd5f7
Tyler Harbert <harbertt11@gmail.com>
2016-04-05 14:03:57 -0400

    converting from JPEG to BMP and BMP to JPEG

--- imagecorrection/tests/img_test.cpp
+++ imagecorrection/tests/img_test.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {   
-   transformGusset("../../images/test_3.bmp", "../../images/test_3_out.bmp");
+    transformGusset("../../images/test_3.bmp", "../../images/test_3_out.bmp");
+
+	JPEG_to_BMP("../../images/foo.jpeg", "../../images/foo.bmp");
+	BMP_to_JPEG("../../images/foo.bmp", "../../images/foo.jpeg");
 
     return 0;
 }
