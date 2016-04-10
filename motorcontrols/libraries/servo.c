518ac92da80f85b51f7da4c0a32f73675a92f751
Team K <harbertt11+teamk@gmail.com>
2016-04-10 21:46:09 +0000

    making image bmp and changing image save names

--- motorcontrols/libraries/servo.c
+++ motorcontrols/libraries/servo.c
@@ -217,7 +217,7 @@ void Cap_Image()
 {
     int n=75;
     int cx=0;
-    static int i=6;
+    static int i=0;
     char command[n];
     cx=snprintf(command, n, "fswebcam -r 2592x1944 --jpeg 100 -D 15 -S 13 1 ../images/testing%d.jpeg", i); //assigns the echo call as the command, with the limit of n characters
     if(cx>n)
