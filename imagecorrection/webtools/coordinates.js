1e318644ea13716d6eb8e77b4350adbfa1ef719e
Tyler Harbert <harbertt11@gmail.com>
2016-04-12 11:27:45 -0400

    adding a locked position state to the web tool

--- imagecorrection/webtools/coordinates.js
+++ imagecorrection/webtools/coordinates.js
@@ -1,8 +1,23 @@
 $(function(){
+    var locked = false;
+    
     // updates coordinates
     $('#image').mousemove(function(e){
-        var offset = $(this).offset();
-        $('#coordvals').html('Coordinates: x: ' + (e.pageX - offset.left) + ' y : ' + (1944 - (e.pageY - offset.top)));
+        if (!locked) {
+            var offset = $(this).offset();
+            $('#coordvals').html('Coordinates: x: ' + (e.pageX - offset.left) + ' y : ' + (1944 - (e.pageY - offset.top)));
+        }
+    });
+    
+    // lock current position
+    $('#image').click(function(){
+        if (locked) {
+            locked = false;
+            $('#coordvals').removeClass('bold');
+        } else {
+            locked = true;
+            $('#coordvals').addClass('bold');
+        }
     });
     
     // updates the image src
